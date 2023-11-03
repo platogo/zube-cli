8bb4a9120a259ed8d3eaec0a41a436f2f999b032
Daniils Petrovs <daniils@platogo.com>
2023-11-03 12:06:05 +0100

    Release `0.3.2`

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const Version = "0.3.1"
+const Version = "0.3.2"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
